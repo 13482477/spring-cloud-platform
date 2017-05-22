24700a078db72f1d39c15eb4859ae7a52f292f57
lizhiqiang <lizhiqiang@DESKTOP-07T1GS1>
2017-05-22 11:46:18 +0800

    commit

---
+++ spring-cloud-message-consumer/src/main/java/com/siebre/messagedemo/remoteservice/MessageObjectFallback.java
@@ -0,0 +1,16 @@
+package com.siebre.messagedemo.remoteservice;
+
+import java.util.List;
+
+import com.siebre.basic.web.WebResult;
+import com.siebre.messagedemo.entity.MessageObject;
+
+public class MessageObjectFallback implements MessageObjectRemoteService {
+
+	@Override
+	public WebResult<List<MessageObject>> find(int page, int limit, String sortField, String order) {
+		return WebResult.<List<MessageObject>>builder().returnCode("500").returnMessage("服务调用失败").build();
+	}
+	
+
+}
---
+++ spring-cloud-message-consumer/src/test/java/com/siebre/message/test/controller/messageobject/MessageObjectControllerTest.java
@@ -0,0 +1,37 @@
+package com.siebre.message.test.controller.messageobject;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.siebre.message.test.base.WebApplicationMockConfig;
+import com.siebre.messagedemo.service.messageobject.MessageObjectService;
+
+public class MessageObjectControllerTest extends WebApplicationMockConfig {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@InjectMocks
+	private MessageObjectService messageObjectService;
+	
+	
+	@Test
+	public void listTest() throws Exception {
+		MockMvc mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+		MvcResult result = mvc.perform(MockMvcRequestBuilders
+				.get("/api/v1/messageObject/1")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn();
+		result.getResponse().getContentAsString();
+	}
+
+}
