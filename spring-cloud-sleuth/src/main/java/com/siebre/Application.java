d42567e18adbc10f2dfe7c6d5aec3a3b22646385
lizhiqiang <lizhiqiang@DESKTOP-07T1GS1>
2017-05-23 15:24:06 +0800

    commit

--- spring-cloud-sleuth/src/main/java/com/siebre/Application.java
+++ spring-cloud-sleuth/src/main/java/com/siebre/Application.java
@@ -1,4 +1,4 @@
-package com.siebre.spring.cloud.sleuth;
+package com.siebre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZipkinStreamServer
-public class ZipkinServerApplication {
+public class Application {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(ZipkinServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 	
 }
