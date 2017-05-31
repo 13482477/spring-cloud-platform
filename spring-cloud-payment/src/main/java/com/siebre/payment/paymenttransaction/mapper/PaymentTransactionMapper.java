0d1ca172eefb9fd6be49b00876a7397bf8e91f08
lizhiqiang <lizhiqiang@DESKTOP-07T1GS1>
2017-05-31 17:56:47 +0800

    c

--- spring-cloud-payment/src/main/java/com/siebre/payment/paymenttransaction/mapper/PaymentTransactionMapper.java
+++ spring-cloud-payment/src/main/java/com/siebre/payment/paymenttransaction/mapper/PaymentTransactionMapper.java
@@ -1,8 +1,48 @@
 package com.siebre.payment.paymenttransaction.mapper;
 
+import java.util.Date;
+import java.util.List;
+
+import com.siebre.payment.entity.enums.PaymentInterfaceType;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.siebre.basic.query.PageInfo;
+import com.siebre.payment.entity.enums.PaymentTransactionStatus;
+import com.siebre.payment.paymenttransaction.entity.PaymentTransaction;
+
 @Repository
-public class PaymentTransactionMapper {
+public interface PaymentTransactionMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PaymentTransaction record);
+
+    int insertSelective(PaymentTransaction record);
+
+    PaymentTransaction selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(PaymentTransaction record);
+
+    int updateByPrimaryKey(PaymentTransaction record);
+
+    public List<PaymentTransaction> queryPaymentTransaction(
+            @Param("orderNumber") String orderNumber,
+            @Param("applicantNumber") String applicantNumber,
+            @Param("status") PaymentTransactionStatus status,
+            @Param("interfaceType") PaymentInterfaceType type,
+            @Param("channelId") Long channelId,
+            @Param("startDate") Date startDate,
+            @Param("endDate") Date endDate,
+            @Param("pageInfo") PageInfo pageInfo
+    );
+
+    PaymentTransaction selectByInterTradeNo(String internalTn);
+
+    List<PaymentTransaction> selectByPaymentOrderId(Long id);
+
+    int updateTransactionStatusToClose(String orderNumber);
+
+    List<PaymentTransaction> selectTransaction(@Param("orderId") Long orderId,
+                                                                            @Param("interfaceType") PaymentInterfaceType interfaceType);
 
 }
--- spring-cloud-payment/src/main/java/com/siebre/payment/paymenttransaction/service/PaymentTransactionService.java
+++ spring-cloud-payment/src/main/java/com/siebre/payment/paymenttransaction/service/PaymentTransactionService.java
@@ -3,7 +3,7 @@ package com.siebre.payment.paymenttransaction.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.siebre.payment.mapper.paymenttransaction.PaymentTransactionMapper;
+import com.siebre.payment.paymenttransaction.mapper.PaymentTransactionMapper;
 
 @Service
 public class PaymentTransactionService {
