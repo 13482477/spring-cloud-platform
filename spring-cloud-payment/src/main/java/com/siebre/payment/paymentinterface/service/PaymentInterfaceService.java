90f99663990164a895b9d27a4f3573d2f71bdf68
lizhiqiang <lizhiqiang@DESKTOP-07T1GS1>
2017-06-01 14:26:14 +0800

    commit

--- spring-cloud-payment/src/main/java/com/siebre/payment/paymentinterface/service/PaymentInterfaceService.java
+++ spring-cloud-payment/src/main/java/com/siebre/payment/paymentinterface/service/PaymentInterfaceService.java
@@ -3,8 +3,10 @@ package com.siebre.payment.paymentinterface.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.siebre.payment.paymentinterface.mapper.PaymentInterfaceMapper;
+
 @Service
-public class PaymentInterfaceMapper {
+public class PaymentInterfaceService {
 	
 	@Autowired
 	private PaymentInterfaceMapper paymentInterfaceMapper;
