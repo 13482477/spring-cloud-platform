b3ca966d70dff5e42594cccfc952e7e48e7157f0
zhiqiang.li <zhiqiang.li@siebre.com>
2017-06-09 13:30:16 +0800

    c

--- spring-cloud-gateway/src/main/java/com/siebre/Application.java
+++ spring-cloud-gateway/src/main/java/com/siebre/Application.java
@@ -3,19 +3,12 @@ package com.siebre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ImportResource;
 
+@SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class, DataSourceAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
-@ImportResource({"classpath:spring/applicationContext-*.xml"})
 public class Application {
 	
 	public static void main(String[] args) {
