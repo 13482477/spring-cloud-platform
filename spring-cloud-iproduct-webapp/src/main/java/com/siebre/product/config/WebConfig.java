08c8330309f221e4258bfacd6b30600e7fcc3d00
huangfei <fei.huang@siebre.com>
2017-06-22 18:03:08 +0800

    Add open session in view filter

--- spring-cloud-iproduct-webapp/src/main/java/com/siebre/product/config/WebConfig.java
+++ spring-cloud-iproduct-webapp/src/main/java/com/siebre/product/config/WebConfig.java
@@ -1,11 +1,11 @@
 package com.siebre.product.config;
 
-import javax.servlet.Filter;
-
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.orm.hibernate4.support.OpenSessionInViewFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
@@ -13,6 +13,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -32,6 +34,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
         return characterEncodingFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean MyFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new OpenSessionInViewFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
     }
 
 
