a9a1e8db79aaf55bf7872f79243eed4c39e04bc1
huangfei <fei.huang@siebre.com>
2017-06-27 17:20:53 +0800

    Change class loader overriding to type inherit for Repository

---
+++ spring-cloud-iproduct-webapp/src/main/java/com/siebre/repository/entity/SiebreCloudRepositoryInitializer.java
@@ -0,0 +1,19 @@
+package com.siebre.repository.entity;
+
+import com.siebre.repository.Repository;
+
+
+/**
+ * Service used to initialize {@link Repository repositories} with EntityProviders.
+ *
+ * @author ZhangChi
+ * @since 2012-12-18
+ *
+ */
+public class SiebreCloudRepositoryInitializer extends RepositoryInitializer{
+
+    @Override
+    public void initialize() {
+        //Do nothing
+    }
+}
