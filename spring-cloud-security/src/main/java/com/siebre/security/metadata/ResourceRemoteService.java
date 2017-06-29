f3fc2cef72f7504689bb71869b79e00fcd42e5ec
zhiqiang.li <zhiqiang.li@siebre.com>
2017-06-29 11:22:31 +0800

    commit

--- spring-cloud-security/src/main/java/com/siebre/security/metadata/ResourceRemoteService.java
+++ spring-cloud-security/src/main/java/com/siebre/security/metadata/ResourceRemoteService.java
@@ -3,7 +3,6 @@ package com.siebre.security.metadata;
 import java.util.List;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +10,6 @@ import com.siebre.basic.web.WebResult;
 import com.siebre.security.entity.Resource;
 
 @FeignClient(value = "SPRING-CLOUD-UAA", fallback = ResourceRemoteServiceFallback.class)
-@Order(1)
 public interface ResourceRemoteService {
 	
 	@RequestMapping(value ="/api/v1/resourcesAndPermission", method = {RequestMethod.GET})
